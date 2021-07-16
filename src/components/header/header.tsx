9cb847485e153e87688477db4bd7686ea24e7362
NandosJake <39186454+NandosJake@users.noreply.github.com>
2021-07-16 15:42:53 +0100

    feat: added header component

---
+++ src/components/header/header.tsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const Header: React.FC = () => (
+  <div>
+    <h1 className="pb-16 pt-16 text-6xl text-center tracking-wide font-mono text-gray-800 font-bold">
+      ORDERS UI
+    </h1>
+  </div>
+);
+
+export default Header;
