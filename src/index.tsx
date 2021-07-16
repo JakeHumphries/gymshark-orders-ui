dfd8d3695200375617bd3bf0ca6e5e435110a1b0
NandosJake <39186454+NandosJake@users.noreply.github.com>
2021-07-16 15:43:34 +0100

    feat: updated index

--- src/index.tsx
+++ src/index.tsx
@@ -1,17 +1,16 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+import Header from './components/header/header';
+import TextInput from './components/text-input/text-input';
+import Table from './components/table/table';
 
 ReactDOM.render(
   <React.StrictMode>
-    <App />
+    <Header />
+    <TextInput buttonName="Submit" placeholderText="Enter Order Quantity" />
+    <Table />
+    <TextInput buttonName="Add" placeholderText="Add New Pack Size" />
   </React.StrictMode>,
-  document.getElementById('root')
+  document.getElementById(`root`),
 );
-
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
