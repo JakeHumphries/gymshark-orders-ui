6bfac8b15326a58e6b62a55cfbc1d620ddb120e0
NandosJake <39186454+NandosJake@users.noreply.github.com>
2021-07-19 11:24:38 +0100

    feat: added get pack breakdown call to use effect

--- src/components/main-wrapper/main-wrapper.tsx
+++ src/components/main-wrapper/main-wrapper.tsx
@@ -1,17 +1,21 @@
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
 import Header from '../header/header';
 import Table from '../table/table';
 import OrderSubmit from '../order-submit/order-submit';
 import AddPack from '../add-pack/add-pack';
+import { getPackBreakdown } from '../../api/orders-api';
 
 const MainWrapper: React.FC = () => {
-  const [generatedPacks, updateGeneratedPacks] = useState({
-    250: 0,
-    500: 0,
-    1000: 0,
-    2000: 0,
-    5000: 0,
-  });
+  const [generatedPacks, updateGeneratedPacks] = useState({});
+
+  useEffect(() => {
+    const getPacks = async () => {
+      const packBreakdown = await getPackBreakdown(`0`);
+      updateGeneratedPacks(packBreakdown);
+    };
+    getPacks();
+  }, []);
+
   return (
     <>
       <Header />
@@ -20,7 +24,10 @@ const MainWrapper: React.FC = () => {
         placeholderText="Enter Order Quantity"
         updateGeneratedPacks={updateGeneratedPacks}
       />
-      <Table generatedPacks={generatedPacks} />
+      <Table
+        generatedPacks={generatedPacks}
+        updateGeneratedPacks={updateGeneratedPacks}
+      />
       <AddPack
         buttonName="Add"
         placeholderText="Add New Pack Size"
